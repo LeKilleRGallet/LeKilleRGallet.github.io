cafd07b736f170a7025a9aab2364afe8a3660acb
Augusto Rico <arico@unal.edu.co>
2026-09-18 13:21:52 -0500

    Actualizar src/pages/sitemap.xml.ts

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,9 +1,17 @@
 import { getCollection } from "astro:content";
+import { publicProfile } from "../data/publicProfile";
+
+const esc = (value: string) =>
+  value
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;");
 
 export const GET = async ({ site }: any) => {
   const proyectos = await getCollection("proyectos", ({ data }) => data.public);
 
-  const paths = [
+  const staticPages = [
     "/",
     "/investigacion/",
     "/research/",
@@ -15,13 +23,30 @@ export const GET = async ({ site }: any) => {
     "/cv/",
     "/sobre-mi/",
     "/contacto/",
-    ...proyectos.map((p) => `/proyectos/${p.id}/`),
-  ];
+  ].map((path) => ({
+    path,
+    lastmod: publicProfile.lastUpdated,
+  }));
 
-  const urls = paths
-    .map((path) => {
+  const projectPages = proyectos.map((p) => ({
+    path: `/proyectos/${p.id}/`,
+    lastmod: p.data.updated.toISOString().slice(0, 10),
+  }));
+
+  const pdfPages = proyectos
+    .filter((p) => Boolean(p.data.pdf))
+    .map((p) => ({
+      path: p.data.pdf!,
+      lastmod: p.data.updated.toISOString().slice(0, 10),
+    }));
+
+  const urls = [...staticPages, ...projectPages, ...pdfPages]
+    .map(({ path, lastmod }) => {
       const loc = new URL(path, site).toString();
-      return `  <url><loc>${loc}</loc></url>`;
+      return `  <url>
+    <loc>${esc(loc)}</loc>
+    <lastmod>${lastmod}</lastmod>
+  </url>`;
     })
     .join("\n");
 
@@ -34,6 +59,7 @@ ${urls}
   return new Response(body, {
     headers: {
       "Content-Type": "application/xml; charset=utf-8",
+      "Cache-Control": "public, max-age=3600",
     },
   });
 };
